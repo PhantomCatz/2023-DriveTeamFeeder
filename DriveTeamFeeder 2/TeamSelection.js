c3fadd96448dfb70aec03be710f437143a46a262
Daniel Huang <danielhuang803@gmail.com>
2023-02-22 10:34:39 -0800

    DH 2/22

--- DriveTeamFeeder 2/TeamSelection.js
+++ DriveTeamFeeder 2/TeamSelection.js
@@ -1,213 +1,39 @@
-
-const searchBar = document.getElementById("search-bar");
-const searchButton = document.getElementById("search-button");
-
-searchButton.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  const query = searchBar.value;
-
-  if (query == "199" || query == "Deep Blue") {
-    location.href = "DataPage.html";
-  }
-  else if (query == "253")//|| query == "Boba Bots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "254")//|| query == "The Cheesy Poofs")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "701")//|| query == "RoboVikes")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "751") //|| query == "barn2robotics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "766")//|| query == "M-A Bears")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "1076")//|| query == "Harker Robotics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "1280")//|| query == "Ragin' C-Biscuits")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "1458")//|| query == "Red Tie Robotics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "1662")//|| query == "Raptor Force")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "1662")//|| query == "Raptor Force Engineering")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "1678")//|| query == "Citrus Circuits")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2035")//|| query == "Robo Rockin' Bots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2141")// || query == "Spartonics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2204")// || query == "Rambots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2288")// || query == "Hydra Robotics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2367")//|| query == "Lancer Robotics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2489")//|| query == "The Insomniacs")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2551")// || query == "Penguin Empire")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "2637")// || query == "Phantom Catz")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "3189")// || query == "Circuit Breakers")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "3257")//|| query == "Vortechs")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "3482")//|| query == "Arrowbotics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "3501")// || query == "Firebots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "3598")//|| query == "SEStematic Eliminators")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "4135")// || query == "Iron Patriots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "4159") // || query == "Iron Patriots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "4643") // || query == "Butte Builts Bots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "5274") // || query == "Wolverines")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "5458") //|| query == "Digital Minds")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "5700") // || query == "SOTA Cyberdragons")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "5728") //|| query == "MC2")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "6619") //|| query == "GravitechX")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "6662") //|| query == "FalconX")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "6941") //|| query == "IronPulse Robotics")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "6981") //|| query == "Clockwork Concord")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "7137") //|| query == "Project 212")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "7401") // || query == "DevTech117")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "7528") // || query == "Nuts and Bolts")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "7667") // || query == "OtterBots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "7686") // || query == "Acabots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "8091") // || query == "The Condors")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "8404") //  || query == "SKYWALKERS")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "8768") // || query == "CTEC Titans")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "8793")//|| query == "Pumpkin Bots")
-  {
-    location.href = "DataPage.html";
-  }
-  else if (query == "9114")//|| query == "Rotationaries")
-  {
-    location.href = "DataPage.html";
-  }
-  else {
-    location.href = "Menu.html";
-  }
-});
-
-//function dis
-/*
-export default function team({ query }) {
-    return(
-      query
-    )
-  }
-
-class teams
+function compareTeams()
 {
-    static get name()
+  const authKey = '0iSKwn3ykkgDT9ToHqwBizSiiaa44pyLIK85oEdgOkzxNJS1X0vBtDFrJ24PiAWW';
+  let teamNumber = document.getElementById("teamNumber").value;
+  let eventcode = '2023cada';
+  // const eventCode = '2022cada';
+
+  fetch('https://www.thebluealliance.com/api/v3/event/' +
+      eventcode +
+      '/teams/simple',
     {
-      return query; 
-    }
+      method: "GET",
+      headers: {
+        'X-TBA-Auth-Key':
+          '0iSKwn3ykkgDT9ToHqwBizSiiaa44pyLIK85oEdgOkzxNJS1X0vBtDFrJ24PiAWW',
+      },
+    })
+    .then(response => response.json())
+    .then(teams => {
+      let team = teams.find(team => team.team_number == teamNumber);
+      if (!team) 
+      {
+        document.getElementById("display").innerHTML = "No team found"
+        return;
+      }
+      // else{
+      // document.getElementById("display").innerHTML = teamNumber;
+      location.href = "DataPage.html";
+          
+    })
+    .catch(error => console.error(error));
+
+    //document.getElementById("team number").innerHTML += teamNumber;
 }
-module.export = new team_Name();*/
+function teamNum()
+{
+  
+}
+        
